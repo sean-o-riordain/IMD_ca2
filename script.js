aaa58b324a760759133c4253887f55d27dd8bb13
X00154002 Sean O Riordain <X00154002@ta.it-tallaght.ie>
2021-12-02 15:51:50 +0000

    Alert

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 var scrollSpy = new bootstrap.ScrollSpy(document.body, { target: '#navbar' })
 
 function sendContact() {
-  
   alert("Your Message has been sent!");
+  
+
+  
+}
+
+function alert(message, type) {
+  document.getElementById('liveAlertPlaceholder').innerHTML = "";
+  var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+  var wrapper = document.createElement('div')
   document.getElementById('emailInput').value= "";
   document.getElementById('subjectInput').value= "";
   document.getElementById('messageInput').value= "";
+  wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+  console.log(wrapper)
+  alertPlaceholder.append(wrapper)
 
-  
 }
 
 function portfolioModalNext() {
